b841ba4764d93006aa200e1c09dac6355c4a40fa
Karen Tamer <karen.poot@cbtis72.edu.mx>
2022-02-20 22:26:58 -0500

    Enviar datos Intent

--- app/src/main/java/com/example/appclima/Ciudades.kt
+++ app/src/main/java/com/example/appclima/Ciudades.kt
@@ -8,6 +8,9 @@ import android.widget.Button
 import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
+
+    val TAG = "com.vidamrr.appclima.ciudades.CIUDAD"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
@@ -17,11 +20,13 @@ class Ciudades : AppCompatActivity() {
 
         bVeracruz.setOnClickListener(View.OnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "ciudad-orizaba")
             startActivity(intent)
         })
 
         bBerlin.setOnClickListener(View.OnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "ciudad-berlin")
             startActivity(intent)
         })
     }

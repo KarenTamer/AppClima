cefc9d4355cb2e3df66bdfb21f1e0efe95f57dad
Karen Tamer <karen.poot@cbtis72.edu.mx>
2022-03-05 20:51:12 -0500

    App Terminada-Class Main-Network-Weather

--- app/src/main/java/com/example/appclima/Ciudad.kt
+++ app/src/main/java/com/example/appclima/Ciudad.kt
@@ -1,13 +1,14 @@
 package com.example.appclima
 
-class Ciudad (name:String, grados:Int, estatus:String){
+class Ciudad(name:String, weather:ArrayList<Weather>, main:Main){
 
-    var nombre:String = ""
-    var grados:Int = 0
-    var estatus:String = ""
-    init {
-        this.nombre = nombre
-        this.grados = grados
-        this.estatus = estatus
+    var name:String = ""
+    var weather:ArrayList<Weather>? = null
+    var main:Main? = null
+
+    init{
+        this.name = name
+        this.weather = weather
+        this.main = main
     }
 }
---
+++ app/src/main/java/com/example/appclima/Weather.kt
@@ -0,0 +1,10 @@
+package com.example.appclima
+
+class Weather(description:String) {
+
+    var description:String = ""
+
+    init {
+        this.description = description
+    }
+}
